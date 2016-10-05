9b08dc8b5727526339a680d14ca7795078ffa270
Maurilio Atila <cabelotaina@gmail.com>
2016-10-05 13:06:00 -0300

    fix bug on css path

--- includes/wp-divi/renders.php
+++ includes/wp-divi/renders.php
@@ -68,6 +68,6 @@ endif;
 add_action( 'wp_enqueue_scripts', 'wp_divi_makepressure_script' );
 
 function wp_divi_makepressure_script() {
-  wp_register_style( 'divi-makepressure',  plugin_dir_url( __FILE__ ).'includes/wp-divi/ET_Builder_Module_Make_Pressure/frontend/css/ET_Builder_Module_Make_Pressure.css' );
+  wp_register_style( 'divi-makepressure',  plugin_dir_url( __FILE__ ).'ET_Builder_Module_Make_Pressure/frontend/css/ET_Builder_Module_Make_Pressure.css' );
   wp_enqueue_style( 'divi-makepressure' );
 }
